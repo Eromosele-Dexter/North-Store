b683bb8713dc70305e54e87b4a6fa2d16e939562
Dexter Eromosele <dexter25@my.yorku.ca>
2022-06-29 17:11:31 -0400

    impemented responsive hamburgerMenu

--- js/index.js
+++ js/index.js
@@ -1,22 +1,50 @@
 $(document).ready(function () {
 
-    $("#search-bar, .fa-magnifying-glass").mouseenter(function () {
-        $(".shop").hide();
+    toggleHamburgerMenu();
+
+    var mediaQuery = window.matchMedia('(min-width: 1480px)')
+    document.addEventListener('DOMContentLoaded', mediaQuerying)
+    mediaQuerying(mediaQuery)
+
+});
+
+/*------------- FUNCTIONS -------------*/
+
+function togggleLogoView() {
+    $("#search-bar, .fa-magnifying-glass").on("mouseenter", function () {
+        $(".logo").hide();
     });
 
 
-    $("#search-bar, .fa-magnifying-glass").mouseleave(function () {
-        $(".shop").show();
+    $("#search-bar, .fa-magnifying-glass").on("mouseleave", function () {
+        $(".logo").show();
     });
+}
+
+function toggleHamburgerMenu() {
 
     $(".fa-bars").click(function () {
         $('.navbar').toggle();
-        $(this).toggleClass('fa-duotone fa-xmark');
+        $(this).toggleClass("fa-duotone fa-xmark");
     })
 
     $('.navbar, .navbar a').click(function () {
         $('.navbar').hide();
-        $('.fa-bars').removeClass('fa-times');
-    });
+        $('.fa-bars').removeClass("fa-duotone fa-xmark");
+    })
+}
 
-});
+function mediaQuerying(mediaQuery) {
+    if (mediaQuery.matches) {
+        $("#search-bar, .fa-magnifying-glass").mouseenter(function () {
+            $(".logo").off("mouseenter");
+            console.log("if")
+        });
+
+
+    } else {
+        togggleLogoView();
+        console.log("else")
+
+    }
+}
